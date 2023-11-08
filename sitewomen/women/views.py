f6a28d58fdacda7365e6da36a7fcca907a9c32ab
VasilyGorbunov <vasily.gorbunov@gmail.com>
2023-11-08 18:23:30 +0300

    13 - 2:30

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -5,13 +5,19 @@ from django.urls import reverse
 
 menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти", ]
 
+data_db = [
+    {'id': 1, 'title': 'Анджелина Джоли', 'content': 'Биография Анджелина Джоли', 'is_published': True},
+    {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
+    {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулия Робертс', 'is_published': True},
+]
 
 def index(request):
     return render(request,
                   'women/index.html',
                   {
                       'title': 'Главная страница',
-                      'menu': menu
+                      'menu': menu,
+                      'posts': data_db,
                   })
 
 
