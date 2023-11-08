1d69979b1d9d0867822a9c039f97d7d75f8994f2
VasilyGorbunov <vasily.gorbunov@gmail.com>
2023-11-08 13:42:51 +0300

    7 - done

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -6,5 +6,13 @@ def index(request):
     return HttpResponse("Page app women")
 
 
-def categories(request):
-    return HttpResponse("<h1>Posts by categories</h1>")
+def categories(request, cat_id):
+    return HttpResponse(f"<h1>Posts by categories - ID: {cat_id}</h1>")
+
+
+def categories_by_slug(request, cat_slug):
+    return HttpResponse(f"<h1>Posts by categories - SLUG: {cat_slug}</h1>")
+
+
+def archive(request, year):
+    return HttpResponse(f"<h1>Archive by years - YEAR: {year}</h1>")
