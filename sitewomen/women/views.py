9f48dbb507e4464cbcf27d7b1456cefa878a732b
VasilyGorbunov <vasily.gorbunov@gmail.com>
2023-11-12 22:44:51 +0300

    23 -done

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,28 +1,13 @@
-from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect, HttpResponsePermanentRedirect
-from django.shortcuts import render, redirect
-from django.template.loader import render_to_string
-from django.urls import reverse
+from django.http import HttpResponse, HttpResponseNotFound
+from django.shortcuts import render, get_object_or_404
+
+from .models import Women
 
 menu = [
     {'title': 'О сайте', 'url_name': 'about', },
     {'title': 'Добавить статью', 'url_name': 'add_page', },
     {'title': 'Обратная связь', 'url_name': 'contact', },
     {'title': 'Войти', 'url_name': 'login', },
-]
-
-data_db = [
-    {'id': 1, 'title': 'Анджелина Джоли', 'content': '''<h1>Анджели́на Джоли</h1> <p>(англ. Angelina Jolie[5], при рождении <strong>Войт</strong> (англ. Voight), ранее Джоли Питт (англ. Jolie Pitt); род. 4 июня 1975, Лос-Анджелес, Калифорния, США) — американская актриса кино, телевидения и озвучивания, кинорежиссёр, сценаристка, продюсер, фотомодель, посол доброй воли ООН.</p>
-
-<p>Обладательница премии «Оскар», трёх премий «Золотой глобус» (первая актриса в истории, три года подряд выигравшая награду) и двух «Премий Гильдии киноактёров США».</p>
-
-<p>Дебютировала в кино в 1982 году, сыграв роль в комедийном фильме «В поисках выхода» (где снимались также её отец и мать)[6], однако известность получила после того, как сыграла героиню видеоигр Лару Крофт в фильмах «Лара Крофт: Расхитительница гробниц» и «Лара Крофт: Расхитительница гробниц 2 — Колыбель жизни».</p>
-
-В 2009, 2011 и 2013 годах по версии журнала Forbes Джоли была названа самой высокооплачиваемой актрисой Голливуда[7][8].
-
-Её наиболее успешными с коммерческой стороны фильмами стали «Малефисента» (сборы в мировом прокате — 758 миллионов долларов США), «Мистер и миссис Смит» (сборы в мировом прокате — 478 миллионов), «Особо опасен» (341 миллион), «Солт» (293 миллиона ), а также «Турист» (278 миллионов), «Лара Крофт: Расхитительница гробниц» (274 миллиона) и картина с участием Николаса Кейджа «Угнать за 60 секунд» (237 миллионов долларов США)[9].''',
-     'is_published': True},
-    {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
-    {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулия Робертс', 'is_published': True},
 ]
 
 cats_db = [
@@ -33,12 +18,13 @@ cats_db = [
 
 
 def index(request):
+    posts = Women.objects.filter(is_published=True)
     return render(request,
                   'women/index.html',
                   {
                       'title': 'Главная страница',
                       'menu': menu,
-                      'posts': data_db,
+                      'posts': posts,
                       'cat_selected': 0
                   })
 
@@ -56,8 +42,18 @@ def page_not_found(request, exception):
     return HttpResponseNotFound("<h1>PAGE NOT FOUND!!!</h1>")
 
 
-def show_post(request, post_id):
-    return HttpResponse(f'Отображение статьи с Id = {post_id}')
+def show_post(request, post_slug):
+    post = get_object_or_404(Women, slug=post_slug)
+    data = {
+        'title': post.title,
+        'menu': menu,
+        'post': post,
+        'cat_selected': 1
+    }
+
+    return render(request=request,
+                  template_name='women/post.html',
+                  context=data)
 
 
 def addpage(request):
